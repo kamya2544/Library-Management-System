be79f0d9e76a13371bf22441f8c77b149eed687f
Kamya Mehra <113076459+kamya2544@users.noreply.github.com>
2026-02-03 10:09:14 +0530

    Refactor MongoDB connection and CRUD operations

--- database.py
+++ database.py
@@ -1,20 +1,17 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
-# ==============================
 # MongoDB Connection
-# ==============================
 
-MONGO_URI = "mongodb+srv://kamyalms:kamya5268@cluster0.tihr9l5.mongodb.net/?appName=Cluster0"
+MONGO_URI = "mongodb+srv://user:pswd@cluster0.xxxx.mongodb.net/?appName=Cluster0"
 
 client = MongoClient(MONGO_URI)
 
 db = client["library_db"]
 collection = db["books"]
 
-# ==============================
+
 # BOOK CRUD OPERATIONS
-# ==============================
 
 def add_book(title, author, isbn, quantity, category):
     book = {
@@ -26,15 +23,12 @@ def add_book(title, author, isbn, quantity, category):
     }
     collection.insert_one(book)
 
-
 def get_all_books():
     books = []
-    for book in collection.find().sort("title", 1):  # 1 = ascending (A–Z)
+    for book in collection.find().sort("title", 1):  
         book["_id"] = str(book["_id"])
         books.append(book)
     return books
-
-
 
 def update_book(book_id, quantity):
     collection.update_one(
@@ -42,13 +36,12 @@ def update_book(book_id, quantity):
         {"$set": {"quantity": quantity}}
     )
 
-
 def delete_book(book_id):
     collection.delete_one({"_id": ObjectId(book_id)})
-
         
 def add_books_bulk(books):
     collection.insert_many(books)
 
 ##collection.delete_many({})
+
 ##print("✅ All books deleted")
